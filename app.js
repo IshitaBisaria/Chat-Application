65dba13b9307142a237312dc7be7e18e2adb4db5
Ishita <ishitabisaria2011@gmail.com>
2023-09-08 17:07:42 +0530

    Added Signup functionality

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,6 +18,8 @@ const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`The App started on port ${PORT}`);
---
+++ routes/userRoutes.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const path = require("path");
+
+// In your userRoutes.js or other appropriate route file
+router.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
+});
+
+// Registration route
+router.post(
+  "/signup",
+  [
+    body("username")
+      .isLength({ min: 3, max: 30 })
+      .withMessage("Username must be 3-30 characters long"),
+    body("name").notEmpty().withMessage("No Name Provided"),
+    body("email")
+      .isEmail()
+      .withMessage("Invalid email format")
+      .normalizeEmail(),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    // console.log("hello1");
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Destructure user registration data from request body
+      console.log("hello");
+      const { username, name, email, password } = req.body;
+      console.log(username, name, email, password);
+
+      // Check if the email or username already exists in the database
+      const existingUser = await User.findOne({
+        $or: [{ username }, { email }],
+      });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ message: "User with this email or username already exists" });
+      }
+
+      // Hash the password before saving it
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user object with hashed password
+      const newUser = new User({
+        username,
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      // Save the new user to the database
+      await newUser.save();
+
+      return res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+module.exports = router;
