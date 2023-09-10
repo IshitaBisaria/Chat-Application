0ce3246fb54028d4dbcd665ac52cdc9d638b6f44
Ishita <ishitabisaria2011@gmail.com>
2023-09-10 20:28:00 +0530

    Revert "Updated the Models"
    This reverts commit 8f798846dd7adf1d918e2105d9cbb3c53b1b7fcd.

--- models/message.js
+++ models/message.js
@@ -2,29 +2,32 @@ const mongoose = require("mongoose");
 const User = require("./user");
 
 const messageSchema = new mongoose.Schema({
+  messageId: {
+    type: String,
+    required: true,
+  },
   sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type: mongoose.Schema.Types.ObjectId, // Use ObjectId for sender
+    ref: "User", // Reference the "User" model
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId, // Use ObjectId for receiver
+    ref: "User", // Reference the "User" model
     required: true,
   },
   content: {
     type: String,
     required: true,
   },
-  chat: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Chat",
-  },
-  readBy: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
   time: {
     type: Date,
     default: Date.now,
     required: true,
+  },
+  readStatus: {
+    type: Boolean,
+    default: false,
   },
 });
 
--- models/user.js
+++ models/user.js
@@ -1,66 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 3,
-      maxlength: 30,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      match: /^\S+@\S+\.\S+$/,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-    profilePicture: {
-      type: String,
-      required: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    status: {
-      type: Boolean,
-      default: true,
-    },
-    lastActive: {
-      type: Date,
-      default: Date.now,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 30,
   },
-  { timestaps: true }
-);
-
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified) {
-//     next();
-//   }
-
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+    default:
+      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  contactList: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  lastActive: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
